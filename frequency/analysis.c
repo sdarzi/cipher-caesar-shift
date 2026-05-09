345a0fa8c5f2c936f32b939811c4054fc167965d
sdarzi <saeiddarzi@gmail.com>
2026-05-09 18:51:12 +0100

    minor changes to frequency

--- frequency/analysis.c
+++ frequency/analysis.c
@@ -4,27 +4,21 @@
 void count_frequencies(const char *text, int freq[ALPHABET_COUNT]) {
 
     while (*text != '\0') {
-        if (isalpha(*text)) {
+        if (isalpha(*text))
             freq[tolower(*text) - 'a']++;
-            text++;
-        } else {
-            text++;
-        }
+        text++;
     }
 }
 
 int main() {
     int i;
     int freq[ALPHABET_COUNT] = {0};
-    char lower_alphabet[ALPHABET_COUNT] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                               'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                               'w', 'x', 'y', 'z'};
     char plaintext[] = "dani is a gav";
 
     count_frequencies(plaintext, freq);
-    for (i = 0; i < 26; i++) {
+    for (i = 0; i < ALPHABET_COUNT; i++) {
         if (freq[i] != 0)
-            printf("%c: %d\n", lower_alphabet[i], freq[i]);
+            printf("%c: %d\n", 'a' + i, freq[i]);
     }
     return 0;
 }
